673d34abc176e4576a379523c0d024edbc2c67f4
HDanaver <hubai.dani@gmail.com>
2026-05-24 12:41:06 +0200

    made the Search Adapter for Kitsu

--- app/src/main/java/com/example/anime_manga_episode_tracker/PosterImage.java
+++ app/src/main/java/com/example/anime_manga_episode_tracker/PosterImage.java
@@ -1,11 +1,16 @@
 package com.example.anime_manga_episode_tracker;
 
 public class PosterImage{
+
+    private  String original;
+    private String small;
     private String medium;
     private String large;
 
     public String getMedium() {return medium;}
     public String getLarge() {return large;}
+    public String getOriginal() {return original;}
+    public String getSmall() {return small;}
 
 
 }
--- app/src/main/java/com/example/anime_manga_episode_tracker/SearchAdapter.java
+++ app/src/main/java/com/example/anime_manga_episode_tracker/SearchAdapter.java
@@ -1,32 +1,67 @@
 package com.example.anime_manga_episode_tracker;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.anime_manga_episode_tracker.databinding.SearchCardBinding;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
 public class SearchAdapter extends RecyclerView.Adapter<SearchAdapter.ViewHolder> {
 
     @NonNull
     @Override
     public SearchAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.search_card,
+        parent,false);
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull SearchAdapter.ViewHolder holder, int position) {
-
+        holder.setItem(data.get(position));
     }
+
+    private List<KitsuData> data;
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private SearchCardBinding binding;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            binding = SearchCardBinding.bind(itemView);
+            itemView.setOnClickListener(view -> {
+                notifyDataSetChanged();
+            });
+        }
+        public KitsuData getItem(){ return data.get(getAbsoluteAdapterPosition());}
+        public void setItem(KitsuData kitsuData) {
+            binding.textView.setText(kitsuData.getAttributes().getTitle());
+            if (kitsuData.getAttributes().getPoster().getSmall() != null){
+                Picasso.get().load(kitsuData.getAttributes().getPoster().getSmall()).fit().into(binding.imageView);
+            }
+            else if (kitsuData.getAttributes().getPoster().getMedium() != null){
+                Picasso.get().load(kitsuData.getAttributes().getPoster().getMedium()).fit().into(binding.imageView);
+            }
+            else if (kitsuData.getAttributes().getPoster().getLarge() != null){
+                Picasso.get().load(kitsuData.getAttributes().getPoster().getLarge()).fit().into(binding.imageView);
+            }
+            else if (kitsuData.getAttributes().getPoster().getOriginal() != null){
+                Picasso.get().load(kitsuData.getAttributes().getPoster().getOriginal()).fit().into(binding.imageView);
+            }
+            else{
+                Picasso.get().load(R.drawable.ic_launcher_background).fit().into(binding.imageView);
+            }
         }
     }
 }
